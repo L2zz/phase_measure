373761ada913e61ab8e36db027464f3ac92bd203
L2zz <--ulla4571@gmail.com>
2018-11-29 18:21:21 +0900

    UPDATE: Meausre without converge
    Delete all factors to endure convergence
    Make just binary/csv files, no txt.
    Have some error occurs in step whose phase is close to 0 or which is last step

--- apps/get_data.py
+++ apps/get_data.py
@@ -2,11 +2,14 @@ import cmath
 import math
 import sys
 import enum
+import csv
 import numpy as np
 
-#FLUCTUATION_PADDING = 0.1 * math.pi
-#VARIATION_OF_FLUCTUATION_MIN = math.pi - FLUCTUATION_PADDING
-#VARIATION_OF_FLUCTUATION_MAX = math.pi + FLUCTUATION_PADDING
+#
+# VALID_CHECK: Valid phase difference in degree
+#
+STEP_CUT_OFF = 0.1
+VALID_CHECK = 3
 
 VARIATION_OF_START_SIGNAL_MIN = 0.2
 COMPARE_SAMPLE_INTERVAL = 10
@@ -36,50 +39,97 @@ class TargetData(enum.Enum):
     PHASE = 1
 
 #
-# Set target file types to enum
-#
-class TargetFile(enum.Enum):
-
-    ALL = 0
-    CSV = 1
-    TXT = 2
-
-#
-# Compare first step and valid check step which appear after last step end
-# First step and valid check step are controled to have same phase
-#
-#def check_is_valid(first_step, valid_check_step):
-#    
-#    global SAMPLES_PER_STEP
-#
-#    return is_valid
-
-#
 # Make log of data using given file type and data type
 #
-#def make_file(file_name, file_type, data_type):
+def make_file_by_step(file_name, data_type):
+
+    global STEP_CUT_OFF
+    global SAMPLES_PER_STEP
+    
+    if (data_type is TargetData.AMPLITUDE):
+        src = np.fromfile(open('../result/' + file_name + '_amp'), dtype=np.float32)
+        csv_file = open('../csv/' + file_name + '_amp.csv', 'w')
+        csv_wr = csv.writer(csv_file, delimiter=',')
+
+    elif (data_type is TargetData.PHASE):
+        src = np.fromfile(open('../result/' + file_name + '_phase'), dtype=np.float32)
+        csv_file = open('../csv/' + file_name + '_phase.csv', 'w')
+        csv_wr = csv.writer(csv_file, delimiter=',')
+    
+    else:
+        print('\n<< Data type error >>\n')
+        sys.exit()
+    
+    # Get the number of samples to cut off
+    samples_of_step_cut_off = (int)(SAMPLES_PER_STEP * STEP_CUT_OFF)
+    samples_of_cut_off_result = (int)(SAMPLES_PER_STEP * (1.0 - 2.0*(STEP_CUT_OFF)))
+    avg_value = 0
+    for step in range(0, len(src), SAMPLES_PER_STEP):
+        for sample in range(samples_of_step_cut_off, \
+                            SAMPLES_PER_STEP-samples_of_step_cut_off):
+            avg_value += src[step + sample]
+        avg_value /= samples_of_cut_off_result
+        csv_wr.writerow([avg_value])
+        avg_value = 0
+    
+    csv_file.close()
+
 #
-#    global SAMPLES_PER_STEP
+# Compare first step and valid check step which appear after last step 
+# First step and valid check step are controled to have same phase
 #
-#    if (file_type is TargetFile.ALL):
-#
-#    elif (file_type is TargetFile.CSV):
-#
-#    elif (file_type is TargetFile.TXT):
-#
-#    else :
-#        print('\n<< File type error >>\n')
-#        sys.exit()
+def check_is_valid(first_step, valid_check_step):    
+
+    global SAMPLES_PER_STEP
+    global VALID_CHECK
+    
+    # Get the number of samples to cut off
+    samples_of_step_cut_off = (int)(SAMPLES_PER_STEP * STEP_CUT_OFF)
+
+    # Get the steps from result cutting off
+    first_step_cut_off = first_step[samples_of_step_cut_off: -samples_of_step_cut_off]
+    valid_check_step_cut_off = \
+            valid_check_step[samples_of_step_cut_off: -samples_of_step_cut_off]
+    
+    # Get average phase of pulse in step
+    samples_of_cut_off_result = len(first_step_cut_off)
+    avg_phase_first_step = 0
+    avg_phase_valid_check_step = 0
+    for i in range(samples_of_cut_off_result):
+        avg_phase_first_step += cmath.phase(first_step_cut_off[i])
+        avg_phase_valid_check_step += cmath.phase(valid_check_step_cut_off[i])
+    avg_phase_first_step /= samples_of_cut_off_result * 1.0
+    avg_phase_valid_check_step /= samples_of_cut_off_result * 1.0
+    
+    if (avg_phase_first_step < 0):
+        avg_phase_first_step += 2 * math.pi
+    if (avg_phase_valid_check_step < 0):
+        avg_phase_valid_check_step += 2* math.pi
+
+    phase_gap_degree = abs(math.degrees(avg_phase_first_step) - \
+                            math.degrees(avg_phase_valid_check_step))
+    if (phase_gap_degree < VALID_CHECK):
+        is_valid = True
+        print('\n<< Valid >>')
+        print('Phase gap in degree: ' + str(phase_gap_degree) + '\n')
+    else:
+        is_valid = False
+        print('\n<< Unvalid >>')
+        print('Phase gap in degree: ' + str(phase_gap_degree) + '\n')
+    
+    return is_valid
+
 
 #
 # Get start point(sample index) and end point of signal
 # Make target binary file: (file_name)_target
 #
 def detect_target(file_name, sample_rate, total_steps):
-    
+   
     global VARIATION_OF_START_SIGNAL_MIN
     global COMPARE_SAMPLE_INTERVAL
     global BEGIN_CUT_OFF
+    global SAMPLES_PER_STEP
 
     # Set source file's location
     # Set location to read
@@ -125,26 +175,26 @@ def detect_target(file_name, sample_rate, total_steps):
         print('\n<< Fail to detect start pattern >>\n')
         sys.exit()
     
+    # Print the result
+    print('\n<< Success to detect start pattern >>')
+    print('Samples per step: ' + str(SAMPLES_PER_STEP) + '\n')
+    
     # Set end point using start point and total steps
     end_point = start_point + (total_steps) * SAMPLES_PER_STEP
-    src_target = src[start_point:end_point]
-    src_target.tofile('../result/' + file_name + '_target')
-    print(SAMPLES_PER_STEP)
+    
     # Check the target is valid
-    #first_step = src[start_point:start_point+SAMPLES_PER_STEP]
-    #valid_check_step = src[end_point:end_point+SAMPLES_PER_STEP]
-    #if (check_is_valid(first_step, valid_check_step)):
-    #    # Make binary file using target
-    #    src_target = src[start_point:end_point]
-    #    src_target.tofile('../result/' + file_name + '_target')
-    #    print(SAMPLES_PER_STEP)
-    #else:
-    #    print('\n<< Fail to detect end point >>\n')
-    #    sys.exit()
+    first_step = src[start_point: start_point+SAMPLES_PER_STEP]
+    valid_check_step = src[end_point + SAMPLES_PER_STEP: end_point + 2*SAMPLES_PER_STEP]
+    if (check_is_valid(first_step, valid_check_step)):
+        # Make binary file using target
+        src_target = src[start_point:end_point]
+        src_target.tofile('../result/' + file_name + '_target')
+    else:
+        print('\n<< Target is not valid >>\n')
+        sys.exit()
 
-#
-# Make binary/csv/txt files to save amplitude
-# File format: (file_name)_amp, (file_name)_amp.csv, (file_name)_amp.txt
+# Make binary/csv files to save amplitude
+# File format: (file_name)_amp, (file_name)_amp.csv
 #
 def get_amp(file_name, total_steps):
     
@@ -161,19 +211,15 @@ def get_amp(file_name, total_steps):
     amp_list_np = np.asarray(amp_list, dtype=np.float32)
     amp_list_np.tofile('../result/' + file_name + '_amp')
 
-    # Make csv, txt files with steps
-    #make_file(file_name, TargetFile.ALL, TargetData.AMPLITUDE)
+    # Make csv file by steps
+    make_file_by_step(file_name, TargetData.AMPLITUDE)
      
 #
-# Make binary/csv/txt files to save [0, 2pi] phase
-# File format: (file_name)_phase, (file_name)_phase.csv, (file_name)_phase.txt
+# Make binary/csv files to save [0, 2pi] phase in degree
+# File format: (file_name)_phase, (file_name)_phase.csv
 #
 def get_phase(file_name, total_steps):
     
-    #global VARIATION_OF_FLUCTUATION_MIN
-    #global VARIATION_OF_FLUCTUATION_MAX
-    global SAMPLES_PER_STEP
-
     # Set source file's location
     src = np.fromfile(open('../result/' + file_name + '_target'), dtype=np.complex64)
     
@@ -190,23 +236,15 @@ def get_phase(file_name, total_steps):
         if (phase < 0):
             phase += 2 * math.pi
         
-        # Detect fluctuation caused by ON/OFF
-        #variation_of_phase = abs(phase - prev_phase)
-        #if ((variation_of_phase > VARIATION_OF_FLUCTUATION_MIN) and \
-        #    (variation_of_phase < VARIATION_OF_FLUCTUATION_MAX)):
-        #        phase -= math.pi
-        #        if (phase < 0):
-        #            phase += 2 * math.pi
-
-        phase_list.append(phase)
+        phase_list.append(math.degrees(phase))
         prev_phase = phase
 
     # Make binary file
     phase_list_np = np.asarray(phase_list, dtype=np.float32)
     phase_list_np.tofile('../result/' + file_name + '_phase')
 
-    # Make csv, txt files with steps
-    #make_file(file_name, TargetFile.ALL, TargetData.PHASE)
+    # Make csv file by steps
+    make_file_by_step(file_name, TargetData.PHASE)
     
 #
 # Call get_amp & get_phase to get amplitude & phase
